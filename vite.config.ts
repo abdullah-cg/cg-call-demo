ed49cbce392dc68ef02037f6e513493f977576c8
abdullahzia <abdullah@cleargrid.co>
2025-09-15 19:42:18 +0500

    added server settings

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // allows access from external IPs
+    allowedHosts: ["summit.cleargrid.ae"], // add your domain
+    port: 5173, // or whatever you're using
+  },
 });
